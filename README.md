46d3fd20ef32c46a567875238613fa7e686d4e84
Ken Nguyen <46609017+kennn1510@users.noreply.github.com>
2025-02-04 18:12:08 +0700

    Set up initial template

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-Making a restaurant page using webpack and DOM manipulation
+todo-list
--- src/about.js
+++
@@ -1,9 +0,0 @@
-export default () => {
-  const contentBody = document.querySelector("div#content");
-  const headline = document.createElement("h1");
-
-  headline.textContent =
-    "Hi, not much here, a bit lazy to get this done, but this is the about page. If you see this message, then my code here is working.";
-
-  contentBody.appendChild(headline);
-};
